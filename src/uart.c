8bd9f4024bdebc108a9b9ee1f5bb8f392a251b5e
Wouter Dijkstra <wouter-dijkstra@hotmail.com>
2012-05-24 16:46:08 +0200

    Uarts configured

--- src/uart.c
+++ src/uart.c
@@ -1,10 +1,11 @@
 #include "uart.h"
 #include "device.h"
+#include "oscillator.h"
 
 void init_rps(void)
 {
 
-	RPOR1bits.RP3R = 3;		//U1TX
+	RPOR1bits.RP3R = 3;		// U1TX
 	RPINR18 = 4;			// U1RX
 	RPOR3bits.RP6R = 5;		// U2TX
 	RPINR19 = 7;			// U2RX
@@ -12,11 +13,35 @@ void init_rps(void)
 	RPINR17 = 9;			// U3RX
 	RPOR5bits.RP10R = 30;	// U4TX
 	RPINR27 = 17; 			// U4RX
-
 	
 }
 
 void initialize_uarts(void)
 {
- init_rps();
+ 	init_rps();
+
+	U1MODEbits.UARTEN = 0;
+	U2MODEbits.UARTEN = 0;
+	U3MODEbits.UARTEN = 0;
+	U4MODEbits.UARTEN = 0;
+
+	U1BRG = U2BRG = U3RBG = U4BRG = (FCY / (16 * BAUD))-1;
+
+	U1STAbits.URXISEL = 2;
+	U2STAbits.URXISEL = 2;
+	U3STAbits.URXISEL = 2;
+	U4STAbits.URXISEL = 2;
+	
+	U1STAbits.UTXEN = 1;
+	U2STAbits.UTXEN = 1;
+	U3STAbits.UTXEN = 1;
+	U4STAbits.UTXEN = 1;
+
+	U1MODEbits.UARTEN = 1;
+	U2MODEbits.UARTEN = 1;
+	U3MODEbits.UARTEN = 1;
+	U4MODEbits.UARTEN = 1;
+
+	
+
 }
--- src/uart.h
+++ src/uart.h
@@ -1,6 +1,8 @@
 #ifndef _SRC_UART_H
 #define _SRC_UART_H
 
+#define BAUD 38400
+
 void initialize_uarts(void);
 
 #endif /* src/uart.h */
