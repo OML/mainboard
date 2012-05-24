4f9c31d4094615131657499282eba875bedb0785
Leon Colijn <lcolijn@xs4all.nl>
2012-05-24 10:54:13 +0200

    Add device configuration bits

---
+++ src/oscillator.c
@@ -0,0 +1,2 @@
+#include "oscillator.h"
+
---
+++ src/oscillator.h
@@ -0,0 +1,9 @@
+#ifndef _SRC_OSCILLATOR_H
+#define _SRC_OSCILLATOR_H
+
+#define F_CLK 4000000
+#define FCY (F_CLK/2)
+
+void configure_oscilator(void);
+
+#endif /* src/oscillator.h */
