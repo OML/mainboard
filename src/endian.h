306b2b36655f93215bb546ba96415ae341f384dd
Leon Colijn <lcolijn@xs4all.nl>
2012-05-29 09:25:32 +0200

    Add endiannes conversion functions for little endian

---
+++ src/endian.h
@@ -0,0 +1,14 @@
+#ifndef _ENDIAN_H
+#define _ENDIAN_H
+
+#define htole16(v) \
+        (v)
+#define htole32(v) \
+        (v)
+
+#define le16toh(v) \
+        (v)
+#define le32toh(v) \
+        (v)
+
+#endif /* endian.h */
